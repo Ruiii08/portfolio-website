7edbe381b20fb899915870a9f5a9db3cccf2e61b
Ruiii08 <145280655+Ruiii08@users.noreply.github.com>
2024-06-15 20:15:02 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+function menu(){
+    const item = document.querySelector('.ham_links');
+    const icon = document.querySelector('.ham_icon');
+    item.classList.toggle("open")
+    icon.classList.toggle("open")
+
+}
